e8764aa6c177af7d99d6d95bb49134ba6c16ccf3
Kangeldi Eginbaev <han.eginbaev@gmail.com>
2023-05-26 15:18:52 +0600

    create items object

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {nanoid} from "nanoid";
 import './App.css';
 
 function App() {
+
+  const createItems = () => {
+    const items: { hasItem: boolean, clicked: boolean, id: string }[] = []
+    for (let i = 0; i < 36; i++) {
+      items.push({hasItem: false, clicked: false, id: nanoid(7)})
+    }
+
+    const index = Math.floor(Math.random() * items.length)
+    items[index].hasItem = true;
+    return items;
+  }
+
+
+  const [items, setItems] = useState(createItems());
+  console.log(items);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
